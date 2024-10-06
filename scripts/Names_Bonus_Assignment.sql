2f1e3ecdc1f3f08310137e8a528242b777fb670e
JesseCrawford89 <jesse.s.crawford89@gmail.com>
2024-10-05 21:42:58 -0500

    Finished question 10; 50% done

--- scripts/Names_Bonus_Assignment.sql
+++ scripts/Names_Bonus_Assignment.sql
@@ -45,3 +45,31 @@ FROM names
 GROUP BY gender
 
 --Answer: There are more females registered.
+
+--Question 8: What are the most popular male and female names overall (i.e., the most total registrations)?
+
+SELECT DISTINCT n.name, gender, SUM(n.num_registered) AS total_registered
+FROM names AS n
+GROUP BY DISTINCT n.name, gender
+ORDER BY total_registered DESC
+
+--Answer: James is the most popular male name and Mary is the most popular female name.
+
+--Question 9: What are the most popular boy and girl names of the first decade of the 2000s (2000 - 2009)?
+
+SELECT DISTINCT n.name, gender, SUM(n.num_registered) AS total_registered
+FROM names AS n
+WHERE n.year BETWEEN 2000 AND 2009
+GROUP BY DISTINCT n.name, gender
+ORDER BY total_registered DESC
+
+--Answer: Jacob is the most popular male name during the first decade of the 2000's, and Emily is the most popular female name of the first decade of the 2000's.
+
+--Question 10: Which year had the most variety in names (i.e. had the most distinct names)?
+
+SELECT n.year, COUNT(n.year) AS number_of_names
+FROM names AS n
+GROUP BY n.year
+ORDER BY COUNT(n.year) DESC
+
+--Answer: 2008 had the most variety in names at 35,079.
